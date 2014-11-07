0ef37bcf4eb9ff49b9a3a41c03d5ec1c9fa5328e
Clint Hoagland <c.hoagland@techsmith.com>
2014-11-06 22:20:58 -0500

    Initial Commit

---
+++ CalculatorTests/MyCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests
+{
+   [TestClass]
+   public class MyCalculatorTests
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+      }
+   }
+}
