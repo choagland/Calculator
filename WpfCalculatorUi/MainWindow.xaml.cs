f8a9321af391609db3fa2d716181ac9005bb8f72
Clint Hoagland <c.hoagland@techsmith.com>
2015-11-10 21:10:19 -0500

    added... naive... WPF UI. Not hooked up to do anything...

--- WpfCalculatorUi/MainWindow.xaml.cs
+++ WpfCalculatorUi/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Calculator;
 
 namespace WpfCalculatorUi
 {
@@ -20,9 +21,11 @@ namespace WpfCalculatorUi
    /// </summary>
    public partial class MainWindow : Window
    {
+      private readonly MyCalculator _myCalculator;
       public MainWindow()
       {
          InitializeComponent();
+         _myCalculator = new MyCalculator();
       }
    }
 }
